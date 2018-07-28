4c9313332a6ff2eb61772cb57170f200e3374c53
yue <wuyuebupt@gmail.com>
2018-07-27 17:53:06 -0700

    almost done, need check if pair images are existed or not

--- lib/roi_data_layer/roibatchLoader.py
+++ lib/roi_data_layer/roibatchLoader.py
@@ -66,13 +66,24 @@ class roibatchLoader(data.Dataset):
     minibatch_db = [self._roidb[index_ratio]]
     blobs = get_minibatch(minibatch_db, self._num_classes)
 
-    # data = torch.from_numpy(blobs['data'])
-    # im_info = torch.from_numpy(blobs['im_info'])
+    ###############################################
+    # images 1
+    ###############################################
+
+    # image 1
     data = torch.from_numpy(blobs['data'][0])
     im_info = torch.from_numpy(blobs['im_info'][0])
     # we need to random shuffle the bounding box.
     data_height, data_width = data.size(1), data.size(2)
+
+
+    # image 2
+    data_1 = torch.from_numpy(blobs['data'][1])
+    im_info_1 = torch.from_numpy(blobs['im_info'][1])
+    data_height_1, data_width_1 = data_1.size(1), data_1.size(2)
+    assert (data_height == data_height_1 and data_width == data_width_1)
     if self.training:
+        # image 1 
         # shuffle is used for gt boxes and proposals -> 
         np.random.shuffle(blobs['gt_boxes'][0])
         gt_boxes = torch.from_numpy(blobs['gt_boxes'][0])
@@ -252,17 +263,186 @@ class roibatchLoader(data.Dataset):
         # print (num_boxes)
         # print (proposal_boxes_padding.shape)
         # print (num_proposals)
+        # image 2
+        # shuffle is used for gt boxes and proposals -> 
+        np.random.shuffle(blobs['gt_boxes'][1])
+        gt_boxes_1 = torch.from_numpy(blobs['gt_boxes'][1])
+        proposal_boxes_1 = torch.from_numpy(blobs['offline_proposals'][1])
+
+        ########################################################
+        # padding the input image to fixed size for each group #
+        ########################################################
+
+        # NOTE1: need to cope with the case where a group cover both conditions. (done)
+        # NOTE2: need to consider the situation for the tail samples. (no worry)
+        # NOTE3: need to implement a parallel data loader. (no worry)
+        # get the index range
+
+        # if the image need to crop, crop to the target size.
+        ratio = self.ratio_list_batch[index]
+
+        if self._roidb[index_ratio]['need_crop']:
+            if ratio < 1:
+                # this means that data_width << data_height, we need to crop the
+                # data_height
+                min_y = int(torch.min(gt_boxes_1[:,1]))
+                max_y = int(torch.max(gt_boxes_1[:,3]))
 
 
-        padding_data_2 = padding_data
-        im_info_2 = im_info
-        gt_boxes_padding_2 = gt_boxes_padding
-        num_boxes_2 = num_boxes
-        proposal_boxes_padding_2 = proposal_boxes_padding
-        num_proposals_2 = num_proposals
+                trim_size = int(np.floor(data_width / ratio))
+                if trim_size > data_height:
+                    trim_size = data_height                
+
+                box_region = max_y - min_y + 1
+
+                if min_y == 0:
+                    y_s = 0
+                else:
+                    if (box_region-trim_size) < 0:
+                        y_s_min = max(max_y-trim_size, 0)
+                        y_s_max = min(min_y, data_height-trim_size)
+                        if y_s_min == y_s_max:
+                            y_s = y_s_min
+                        else:
+                            y_s = np.random.choice(range(y_s_min, y_s_max))
+                    else:
+                        y_s_add = int((box_region-trim_size)/2)
+                        if y_s_add == 0:
+                            y_s = min_y
+                        else:
+                            y_s = np.random.choice(range(min_y, min_y+y_s_add))
+                # crop the image
+                data_1 = data_1[:, y_s:(y_s + trim_size), :, :]
+
+                # shift y coordiante of gt_boxes
+                gt_boxes_1[:, 1] = gt_boxes_1[:, 1] - float(y_s)
+                gt_boxes_1[:, 3] = gt_boxes_1[:, 3] - float(y_s)
+
+                proposal_boxes_1[:, 1] = proposal_boxes_1[:, 1] - float(y_s)
+                proposal_boxes_1[:, 3] = proposal_boxes_1[:, 3] - float(y_s)
+
+                # update gt bounding box according the trip
+                gt_boxes_1[:, 1].clamp_(0, trim_size - 1)
+                gt_boxes_1[:, 3].clamp_(0, trim_size - 1)
+
+                proposal_boxes_1[:, 1].clamp_(0, trim_size - 1)
+                proposal_boxes_1[:, 3].clamp_(0, trim_size - 1)
+
+            else:
+                # this means that data_width >> data_height, we need to crop the
+                # data_width
+                min_x = int(torch.min(gt_boxes_1[:,0]))
+                max_x = int(torch.max(gt_boxes_1[:,2]))
+                trim_size = int(np.ceil(data_height * ratio))
+                if trim_size > data_width:
+                    trim_size = data_width                
+                box_region = max_x - min_x + 1
+                if min_x == 0:
+                    x_s = 0
+                else:
+                    if (box_region-trim_size) < 0:
+                        x_s_min = max(max_x-trim_size, 0)
+                        x_s_max = min(min_x, data_width-trim_size)
+                        if x_s_min == x_s_max:
+                            x_s = x_s_min
+                        else:
+                            x_s = np.random.choice(range(x_s_min, x_s_max))
+                    else:
+                        x_s_add = int((box_region-trim_size)/2)
+                        if x_s_add == 0:
+                            x_s = min_x
+                        else:
+                            x_s = np.random.choice(range(min_x, min_x+x_s_add))
+                # crop the image
+                data_1 = data_1[:, :, x_s:(x_s + trim_size), :]
+
+                # shift x coordiante of gt_boxes
+                gt_boxes_1[:, 0] = gt_boxes_1[:, 0] - float(x_s)
+                gt_boxes_1[:, 2] = gt_boxes_1[:, 2] - float(x_s)
+
+                proposal_boxes_1[:, 0] = proposal_boxes_1[:, 0] - float(x_s)
+                proposal_boxes_1[:, 2] = proposal_boxes_1[:, 2] - float(x_s)
+                # update gt bounding box according the trip
+                gt_boxes_1[:, 0].clamp_(0, trim_size - 1)
+                gt_boxes_1[:, 2].clamp_(0, trim_size - 1)
+
+                proposal_boxes_1[:, 0].clamp_(0, trim_size - 1)
+                proposal_boxes_1[:, 2].clamp_(0, trim_size - 1)
+
+        # based on the ratio, padding the image.
+        if ratio < 1:
+            # this means that data_width < data_height
+            trim_size = int(np.floor(data_width / ratio))
+
+            padding_data_1 = torch.FloatTensor(int(np.ceil(data_width / ratio)), \
+                                             data_width, 3).zero_()
+
+            padding_data_1[:data_height, :, :] = data_1[0]
+            # update im_info
+            im_info_1[0, 0] = padding_data_1.size(0)
+            # print("height %d %d \n" %(index, anchor_idx))
+        elif ratio > 1:
+            # this means that data_width > data_height
+            # if the image need to crop.
+            padding_data_1 = torch.FloatTensor(data_height, \
+                                             int(np.ceil(data_height * ratio)), 3).zero_()
+            padding_data_1[:, :data_width, :] = data_1[0]
+            im_info_1[0, 1] = padding_data_1.size(1)
+        else:
+            trim_size = min(data_height, data_width)
+            padding_data_1 = torch.FloatTensor(trim_size, trim_size, 3).zero_()
+            padding_data_1 = data_1[0][:trim_size, :trim_size, :]
+            # gt_boxes.clamp_(0, trim_size)
+            gt_boxes_1[:, :4].clamp_(0, trim_size)
+            proposal_boxes_1[:, :4].clamp_(0, trim_size)
+            im_info_1[0, 0] = trim_size
+            im_info_1[0, 1] = trim_size
+
+
+        # check the bounding box:
+        not_keep = (gt_boxes_1[:,0] == gt_boxes_1[:,2]) | (gt_boxes_1[:,1] == gt_boxes_1[:,3])
+        keep = torch.nonzero(not_keep == 0).view(-1)
+
+        gt_boxes_padding_1 = torch.FloatTensor(self.max_num_box, gt_boxes_1.size(1)).zero_()
+        if keep.numel() != 0:
+            gt_boxes_1 = gt_boxes_1[keep]
+            num_boxes_1 = min(gt_boxes_1.size(0), self.max_num_box)
+            gt_boxes_padding_1[:num_boxes,:] = gt_boxes_1[:num_boxes]
+        else:
+            num_boxes_1 = 0
+ 
+        # check the bounding box - proposal:
+        not_keep_proposal = (proposal_boxes_1[:,0] == proposal_boxes_1[:,2]) | (proposal_boxes_1[:,1] == proposal_boxes_1[:,3])
+        keep_proposal = torch.nonzero(not_keep_proposal == 0).view(-1)
+
+        # proposal_boxes_padding = torch.FloatTensor(self.max_num_box, gt_boxes.size(1)).zero_()
+        # mannual set the number of proposals to 2000
+        proposal_boxes_padding_1 = torch.FloatTensor( 2000, proposal_boxes_1.size(1)).zero_()
+        if keep_proposal.numel() != 0:
+            proposal_boxes_1 = proposal_boxes_1[keep_proposal]
+            num_proposals_1 = min(proposal_boxes_1.size(0), 2000)
+            # print (proposal_boxes.size(), proposal_boxes.size(0), num_proposals)
+            # swith the [bbox, score] to [~, bbox] to follow the rpn output format
+            proposal_boxes_padding_1[:num_proposals_1,1:5] = proposal_boxes_1[:num_proposals_1,:4]
+            proposal_boxes_padding_1[:num_proposals_1, 0] = proposal_boxes_1[:num_proposals_1, 4]
+        else:
+            num_proposals_1 = 0
+ 
+ 
+           # permute trim_data to adapt to downstream processing
+        padding_data_1 = padding_data_1.permute(2, 0, 1).contiguous()
+        im_info_1 = im_info_1.view(3)
+
+
+        # padding_data_2 = padding_data
+        # im_info_2 = im_info
+        # gt_boxes_padding_2 = gt_boxes_padding
+        # num_boxes_2 = num_boxes
+        # proposal_boxes_padding_2 = proposal_boxes_padding
+        # num_proposals_2 = num_proposals
  
         return padding_data, im_info, gt_boxes_padding, num_boxes, proposal_boxes_padding, num_proposals, \
-                padding_data_2, im_info_2, gt_boxes_padding_2, num_boxes_2, proposal_boxes_padding_2, num_proposals_2
+                padding_data_1, im_info_1, gt_boxes_padding_1, num_boxes_1, proposal_boxes_padding_1, num_proposals_1
 
     else:
         data = data.permute(0, 3, 1, 2).contiguous().view(3, data_height, data_width)
