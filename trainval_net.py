2f60264e64bdbb2b3c1ea6d925d1aeef17fe64e4
smilelab <wuyuebupt@gmail.com>
2018-11-12 19:55:17 -0500

    resume and nan handle

--- trainval_net.py
+++ trainval_net.py
@@ -32,6 +32,7 @@ from model.utils.net_utils import weights_normal_init, save_net, load_net, \
 
 from model.faster_rcnn.vgg16 import vgg16
 from model.faster_rcnn.resnet import resnet
+import math
 
 def parse_args():
   """
@@ -225,6 +226,10 @@ def parse_args():
                       default=2, type=int)
 
 
+  parser.add_argument('--resume_dir', dest='resume_dir',
+                      # help='directory to save models', default="/srv/share/jyang375/models",
+                      help='directory to save models', default="/",
+                      type=str)
 
 
 #      loss = RCNN_loss_cls.mean() + 10 * RCNN_loss_bbox.mean() \
@@ -378,9 +383,6 @@ if __name__ == '__main__':
   print ("att_weight           : ", args.att_weight)
   
 
-
-
-
   # parser.add_argument('--neighbor_move', dest='neighbor_move',
   #                     help='confg like 0.3',
   #                     default="0.0", type=float)
@@ -408,6 +410,11 @@ if __name__ == '__main__':
   output_dir = args.save_dir + "/" + args.net + "/" + args.dataset
   if not os.path.exists(output_dir):
     os.makedirs(output_dir)
+
+  if args.resume_dir != '/':
+      output_resume_dir = args.resume_dir + "/" + args.net + "/" + args.dataset
+      if not os.path.exists(output_resume_dir):
+        os.makedirs(output_resume_dir)
 
   sampler_batch = sampler(train_size, args.batch_size)
 
@@ -497,6 +504,53 @@ if __name__ == '__main__':
   elif args.optimizer == "sgd":
     optimizer = torch.optim.SGD(params, momentum=cfg.TRAIN.MOMENTUM)
 
+
+
+  ## auto resume
+  from os import listdir
+  from os.path import isfile, join
+
+  if args.resume_dir != '/':
+      mypath = output_resume_dir
+      onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+  else:
+      onlyfiles = []
+
+  print (onlyfiles)
+  if len(onlyfiles) > 0:
+      ## resume from last
+      sessions = [int(arr.split('_')[2]) for arr in onlyfiles]
+      checkepochs = [int(arr.split('_')[3]) for arr in onlyfiles]
+      checkpoints = [int(arr.split('_')[4].split('.')[0]) for arr in onlyfiles]
+      print (sessions)
+      print (checkepochs)
+      print (checkpoints)
+      lastepoch = max(checkepochs)
+      index = checkepochs.index(lastepoch)
+      lastsession = sessions[index]
+      lastpoints = checkpoints[index]
+      print (lastepoch)
+      print (lastsession)
+      print (lastpoints)
+
+      # load_name = os.path.join(output_dir,
+      load_name = os.path.join(output_resume_dir,
+        'faster_rcnn_{}_{}_{}.pth'.format(lastsession, lastepoch, lastpoints))
+      print("loading checkpoint %s" % (load_name))
+      checkpoint = torch.load(load_name)
+      args.session = checkpoint['session']
+      args.start_epoch = checkpoint['epoch']
+      fasterRCNN.load_state_dict(checkpoint['model'])
+      optimizer.load_state_dict(checkpoint['optimizer'])
+      lr = optimizer.param_groups[0]['lr']
+      if 'pooling_mode' in checkpoint.keys():
+        cfg.POOLING_MODE = checkpoint['pooling_mode']
+      print("loaded checkpoint %s" % (load_name))
+
+
+  ## checkout folder ??
+  ## if nan -> 
+
   if args.resume:
     load_name = os.path.join(output_dir,
       'faster_rcnn_{}_{}_{}.pth'.format(args.checksession, args.checkepoch, args.checkpoint))
@@ -511,6 +565,8 @@ if __name__ == '__main__':
       cfg.POOLING_MODE = checkpoint['pooling_mode']
     print("loaded checkpoint %s" % (load_name))
 
+
+
   if args.mGPUs:
     fasterRCNN = nn.DataParallel(fasterRCNN)
 
@@ -519,7 +575,67 @@ if __name__ == '__main__':
 
   iters_per_epoch = int(train_size / args.batch_size)
 
-  for epoch in range(args.start_epoch, args.max_epochs + 1):
+  nan_flag = False
+
+
+
+  # for epoch in range(args.start_epoch, args.max_epochs + 1):
+  epoch = args.start_epoch - 1
+  print (epoch)
+  
+  # for epoch in range(args.start_epoch, args.max_epochs + 1):
+
+  while True:
+
+    ## if from nan
+    if nan_flag:
+        ## back to the current epoch
+        epoch = epoch - 1  
+        ## reset the flag
+        nan_flag = False
+
+        ## load last model, as we continue, nan model should not be saved
+        mypath = output_dir
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+        print (onlyfiles)
+        if len(onlyfiles) > 0:
+            ## resume from last
+            sessions = [int(arr.split('_')[2]) for arr in onlyfiles]
+            checkepochs = [int(arr.split('_')[3]) for arr in onlyfiles]
+            checkpoints = [int(arr.split('_')[4].split('.')[0]) for arr in onlyfiles]
+            print (sessions)
+            print (checkepochs)
+            print (checkpoints)
+            lastepoch = max(checkepochs)
+            index = checkepochs.index(lastepoch)
+            lastsession = sessions[index]
+            lastpoints = checkpoints[index]
+            print (lastepoch)
+            print (lastsession)
+            print (lastpoints)
+
+            load_name = os.path.join(output_dir,
+              'faster_rcnn_{}_{}_{}.pth'.format(lastsession, lastepoch, lastpoints))
+            print("loading checkpoint %s" % (load_name))
+            checkpoint = torch.load(load_name)
+            # print (checkpoint['model'].keys())
+            # print (fasterRCNN.state_dict().keys())
+            # args.session = checkpoint['session']
+            # args.start_epoch = checkpoint['epoch']
+            # fasterRCNN.load_state_dict(checkpoint['model'])
+            fasterRCNN.load_state_dict({'module.'+k:v for k,v in checkpoint['model'].items()})
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            lr = optimizer.param_groups[0]['lr']
+            if 'pooling_mode' in checkpoint.keys():
+              cfg.POOLING_MODE = checkpoint['pooling_mode']
+            print("loaded checkpoint %s" % (load_name))
+
+    if epoch == args.max_epochs:
+        break
+    epoch = epoch + 1
+   
+
     # setting to train mode
     fasterRCNN.train()
     loss_temp = 0
@@ -723,6 +839,8 @@ if __name__ == '__main__':
           fg_cnt = torch.sum(rois_label.data.ne(0))
           bg_cnt = rois_label.data.numel() - fg_cnt
 
+        
+
         print("[session %d][epoch %2d][iter %4d/%4d] loss: %.4f, lr: %.2e" \
                                 % (args.session, epoch, step, iters_per_epoch, loss_temp, lr), flush=True)
         print("\t\t\tfg/bg=(%d/%d), time cost: %f" % (fg_cnt, bg_cnt, end-start), flush=True)
@@ -731,6 +849,8 @@ if __name__ == '__main__':
         # print("\t\t\trpn_cls: %.4f, rpn_box: %.4f, rcnn_cls_proposal: %.4f, rcnn_box %.4f, bbox_beta %.4f, kl %.4f, cls_alpha %.4f, cls_beta %.4f, kl_cls %.4f" \
         #              % (loss_rpn_cls, loss_rpn_box, loss_rcnn_cls, loss_rcnn_box, loss_rcnn_box_beta, loss_kl, loss_rcnn_cls_alpha, loss_rcnn_cls_beta, loss_kl_cls), flush=True)
         #              % (loss_rpn_cls, loss_rpn_box, loss_rcnn_cls, loss_rcnn_box, 0.0, loss_kl))
+
+        ## 
         if args.use_tfboard:
           info = {
             'loss': loss_temp,
@@ -742,8 +862,19 @@ if __name__ == '__main__':
           for tag, value in info.items():
             logger.scalar_summary(tag, value, step)
 
+        ## loss temp is nan
+        loss_temp = float('nan') 
+        print (loss_temp)
+        if math.isnan(loss_temp):
+            nan_flag = True
+            print ("################NAN in epoch " +str(epoch)+ "########")
+            break
         loss_temp = 0
         start = time.time()
+
+    ## if nan, no save, continue
+    if nan_flag:
+        continue
 
     if args.mGPUs:
       save_name = os.path.join(output_dir, 'faster_rcnn_{}_{}_{}.pth'.format(args.session, epoch, step))
